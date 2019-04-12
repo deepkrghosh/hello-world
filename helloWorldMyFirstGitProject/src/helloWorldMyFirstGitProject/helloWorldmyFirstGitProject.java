b903259b6b5864815393ca7741e53880461d77cc
DEEP <DEEP@Lenovo-PC>
2019-04-13 00:04:57 +0530

    My first hello world Java Git Project

---
+++ helloWorldMyFirstGitProject/src/helloWorldMyFirstGitProject/helloWorldmyFirstGitProject.java
@@ -0,0 +1,10 @@
+package helloWorldMyFirstGitProject;
+
+public class helloWorldmyFirstGitProject {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first GITLAB Project");
+
+	}
+
+}
